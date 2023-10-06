9155ace96f68f9c5ae4367ed23071806221894fe
danlewismuriuki <danlewismuriuki2@gmail.com>
2023-10-05 22:15:10 -0700

    Updated programme to print size of data types in bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %zu\n", sizeof(char));
-	printf("Size of a int: %zu\n", sizeof(int));
-	printf("Size of a long int: %zu\n", sizeof(long));
-	printf("Size of a long long int: %zu\n", sizeof(long long int));
-	printf("Size of a float: %zu\n", sizeof(float));
+	printf("Size of a char: %zu byte(s)", sizeof(char));
+	printf("Size of a int: %zu\n byte(s)", sizeof(int));
+	printf("Size of a long int: %zu\n byte(s)", sizeof(long));
+	printf("Size of a long long int: %zu\n byte(s)", sizeof(long long int));
+	printf("Size of a float: %zu\n byte(s)", sizeof(float));
 	return (0);
 }
