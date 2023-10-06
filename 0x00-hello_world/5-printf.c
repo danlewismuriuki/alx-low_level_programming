a8148e5703fcd855e7e5a9371fee899f14776156
danlewismuriuki <danlewismuriuki2@gmail.com>
2023-10-05 20:27:02 -0700

    Added Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is a program exercise to use printf
+ * Return: Wil return 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
