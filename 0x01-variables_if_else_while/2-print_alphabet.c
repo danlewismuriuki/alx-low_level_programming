f9dbf1ef5bb3cdb0da6e5e64f3040c4610bfbf22
danlewismuriuki <danlewismuriuki2@gmail.com>
2023-10-06 10:47:31 -0700

    A program that prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Enter point
+ *
+ * Return: always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
