240e36681a0311dcdf1fd7d38a834ecf8422bc22
danlewismuriuki <danlewismuriuki2@gmail.com>
2023-10-08 04:31:10 -0700

    Added a program that prints all single digit numbers of base

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the function
+ * Return: is always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
