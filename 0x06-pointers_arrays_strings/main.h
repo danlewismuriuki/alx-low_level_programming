afd77fcf4ceffa10de29174f7fa21119c54a13f6
danlewismuriuki <danlewismuriuki2@gmail.com>
2023-10-19 13:49:57 -0700

    Added a function that concatenates two strings.

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,11 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+#include <stdio.h>
+#include <string.h>
+
 int _putchar(char c);
+char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
