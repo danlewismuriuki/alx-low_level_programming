f620855037f7354fa107a50c5fb4a9b99b038e96
danlewismuriuki <danlewismuriuki2@gmail.com>
2023-10-19 19:08:21 -0700

    Added  a function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,24 +9,23 @@
 
 char *cap_string(char *str)
 {
-    int i, j;
-
-    char c[] = {44, 59, 46, '!', '?', '"', '(', ')', '{', '}', ' ', '\t', '\n'};
-
-    for (i = 0; str[i] != '\0'; i++ )
-    {
-        if (i == 0 && str[i] >= 'a'; && str[i] <= 'z')
-        {
-            str[i] = str[i] - 32;
-        }
-      for (j = 0; c[j] != '\0'; j++ )
-      {
-          if (c[j] == str[i] && (str[i +1] >= 'a'; && str[i + 1]
-          <= 'z')
-          {
-              str[i + 1] = str[i + 1] - 32;
-          }
-      }
-    }
-    return (str);
+	int i, j;
+	
+	char c[] = {44, 59, 46, '!', '?', '"', '(', ')', '{', '}', ' ', '\t', '\n'};
+	
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		for (j = 0; c[j] != '\0'; j++ )
+		{
+			if (c[j] == str[i] && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+		}
+	}
+	return (str);
 }
--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-	char str[] = "Expect the best. \n
-		Prepare for the worst. Capitalize on what comes.\n
-		hello world! hello-world 0123456hello world\t
-		hello world.hello world\n";
+	char str[] = "Expect the best. Prepare for the worst. "
+		"Capitalize on what comes.\nhello world! "
+		"hello-world 0123456hello world\thello world.hello world\n";
+
 	char *ptr;
 
 	ptr = cap_string(str);
