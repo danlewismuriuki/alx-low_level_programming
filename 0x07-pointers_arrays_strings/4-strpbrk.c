712807fa287b26c78b93d23ff9cad166d95b1582
danlewismuriuki <danlewismuriuki2@gmail.com>
2023-10-23 12:29:47 -0700

    Added a function that searches a string for any of a set of bytes.

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strpbrk -This is the ntry point
+ *
+ * @s: String to be searched through
+ *
+ * @accept: String to be used to check string
+ *
+ * Return: Will return 0;
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '0')
+	{
+		const char *a = accept;
+
+		while (*a != '\0')
+		{
+			if (*s == *a)
+			return (s);
+			a++;
+		}
+		s++;
+	}
+	return (NULL);
+}
