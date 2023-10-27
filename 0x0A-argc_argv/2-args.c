c086e642a329c486c5383b990a57f544a5b7d946
danlewismuriuki <danlewismuriuki2@gmail.com>
2023-10-27 14:56:30 -0700

    Added a program that prints all arguments it receives.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument count
+ *
+ * @argv: argument vector
+ *
+ * Return: Return 0;
+ */
+
+int main(int argc, char *argv[])
+{
+	int j;
+
+	for (j = 0; j < argc; j++)
+	{
+		printf("%s\n", argv[j]);
+	}
+	return(0);
+}
