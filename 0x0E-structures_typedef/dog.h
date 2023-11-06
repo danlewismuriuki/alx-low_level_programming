117203fbd40ad108efd0618d7bd0eed4c1ecf2af
danlewismuriuki <danlewismuriuki2@gmail.com>
2023-11-06 13:53:54 -0800

    Added definitions to the header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,27 @@
 #ifndef dog_FILE
 #define dog_FILE
 
+/**
+ * struct dog - Entry for the struct
+ *
+ * @name: dog name
+ *
+ * @age: dog age
+ *
+ * @owner: dog owner
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 
 #endif
