a98ecd19ad28ad9ee734621b06ee2ace5af6d144
danlewismuriuki <danlewismuriuki2@gmail.com>
2023-11-06 16:51:28 -0800

    Added a function that initialize a variable

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -17,10 +17,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if(d != NULL)
+	if (d != NULL)
 	{
-		d -> name = name;
-		d -> age = age;
-		d -> owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
