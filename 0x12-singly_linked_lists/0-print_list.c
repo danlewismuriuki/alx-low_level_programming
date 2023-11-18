6292770cf0e741d8dc05a6e10bba1e9899eff920
danlewismuriuki <danlewismuriuki2@gmail.com>
2023-11-18 04:43:49 -0800

    Updated format specifier

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,7 +14,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%lu] %s\n", h->len, h->str);
 		h = h->next;
 		node_count++;
 	}
