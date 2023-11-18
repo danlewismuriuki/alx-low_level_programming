e9162da0279d17036bdbd1e0db09bdbf1cd0ee28
danlewismuriuki <danlewismuriuki2@gmail.com>
2023-11-18 05:38:15 -0800

    Added a a description on header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,12 +3,19 @@
 
 #include <stddef.h>
 
-typedef struct list_s {
-            char *str;
-            int len;
-            struct list_s *next;
-} list_t;
+/**
+ * struct list_s  -  list struct
+ * @str: String to be checked
+ * @len: The length of the string
+ * @next: Next node
+ */
 
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 size_t print_list(const list_t *h);
 int main(void);
 
