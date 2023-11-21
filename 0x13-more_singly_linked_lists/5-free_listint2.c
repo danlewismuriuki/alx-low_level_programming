50d464d6c65b0beb7d8e0331c11582085c18a17f
danlewismuriuki <danlewismuriuki2@gmail.com>
2023-11-21 05:43:23 -0800

    Added a statememnt to check for null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,6 +12,9 @@ void free_listint2(listint_t **head)
 	listint_t *new_head_node;
 	listint_t *temp_node;
 
+	if (head == NULL)
+		return;
+
 	new_head_node = *head;
 
 	while (new_head_node != NULL)
