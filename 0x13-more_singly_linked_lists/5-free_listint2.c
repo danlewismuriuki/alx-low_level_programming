ae70728f6fa3e85f3a8ca07e443ffcd3d0cb3832
danlewismuriuki <danlewismuriuki2@gmail.com>
2023-11-21 05:11:45 -0800

    Added a function that frees the memory and adds sets the head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - This function free and sets null to the head pointer
+ * @head: Will be set to null
+ * Returns: Returns void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *new_head_node;
+	listint_t *temp_node;
+
+	new_head_node = *head;
+
+	while (new_head_node != NULL)
+	{
+		temp_node = new_head_node;
+		new_head_node = new_head_node->next;
+		free(temp_node);
+	}
+	*head = NULL;
+
+}
