a31480d7839fda980167af3f99b274353ea6e637
danlewismuriuki <danlewismuriuki2@gmail.com>
2023-11-27 18:05:21 -0800

    Added betty code styling

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,32 +10,31 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int file_descriptor, write_result, text_length = 0;
+	int file_descriptor, write_result, text_length = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content == NULL)
-        return (1);
+	if (text_content == NULL)
+		return (1);
 
-    while (text_content[text_length] != '\0')
-        text_length++;
+	while (text_content[text_length] != '\0')
+		text_length++;
 
-    file_descriptor = open(filename, O_WRONLY | O_APPEND);
+	file_descriptor = open(filename, O_WRONLY | O_APPEND);
 
-    if (file_descriptor == -1)
-        return (-1);
+	if (file_descriptor == -1)
+		return (-1);
 
-    write_result = write(file_descriptor, text_content, text_length);
+	write_result = write(file_descriptor, text_content, text_length);
 
-    if (write_result == -1)
-    {
-        close(file_descriptor);
-        return (-1);
-    }
+	if (write_result == -1)
+	{
+		close(file_descriptor);
+		return (-1);
+	}
+	close(file_descriptor);
 
-    close(file_descriptor);
-
-    return (1);
+	return (1);
 }
 
