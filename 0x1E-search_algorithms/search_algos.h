24ff206b465c819d5af5f0647105b21ea0dea455
Dan <danlewismuriuki@gmail.com>
2024-05-12 17:24:35 +0300

    Added a function that searches for a value in a sorted skip list of integers.

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -21,6 +21,24 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
@@ -31,4 +49,6 @@ listint_t *jump_list(listint_t *list, size_t size, int value);
 void print_list(const listint_t *list);
 void free_list(listint_t *list);
 listint_t *create_list(int *array, size_t size);
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
 #endif
